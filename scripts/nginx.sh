4683e7eea55b737898ac68d404b599c10ba4e848
Daniel Hollands <daniel@limeblast.co.uk>
2014-03-26 15:26:29 +0000

    Updated nginx to 1.4

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -2,6 +2,15 @@
 
 echo ">>> Installing Nginx"
 
+# Adding Yum repo
+cat > /etc/yum.repos.d/nginx.repo << EOF
+[nginx]
+name=nginx repo
+baseurl=http://nginx.org/packages/centos/6/x86_64/
+gpgcheck=0
+enabled=1
+EOF
+
 # Installing Nginx
 sudo yum install -y nginx
 
