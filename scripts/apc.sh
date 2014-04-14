a1133aa7f3460e01fb68e99a055789865254dbc0
Adam Smith <adam@schoolstickers.co.uk>
2014-04-14 16:35:44 +0100

    Typo..

--- scripts/apc.sh
+++ scripts/apc.sh
@@ -5,4 +5,4 @@ yum install -y php54w-pecl-apc
 
 # Modify shm_size to cope with Magento
 sed -i "s/apc.shm_size=.*/apc.shm_size=512M/" /etc/php.d/apc.ini
-set -i "s/apc.enable_cli=.*/apc.enable_cli=1/" /etc/php.d/apc.ini
+sed -i "s/apc.enable_cli=.*/apc.enable_cli=1/" /etc/php.d/apc.ini
